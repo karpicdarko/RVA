b232744e4838bf6634cc835780dd9acad0c039a9
KarpicD <karpicdarko1@gmail.com>
2020-08-11 23:43:26 +0200

    Vezbe 8

--- FrontendRVA/src/app/app.module.ts
+++ FrontendRVA/src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
+import { LigaComponent } from './components/liga/liga.component';
+import { IgracComponent } from './components/igrac/igrac.component';
+import { TimComponent } from './components/tim/tim.component';
+import { NacionalnostComponent } from './components/nacionalnost/nacionalnost.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { AutorComponent } from './components/core/autor/autor.component';
+import { AboutComponent } from './components/core/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LigaComponent,
+    IgracComponent,
+    TimComponent,
+    NacionalnostComponent,
+    HomeComponent,
+    AutorComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
